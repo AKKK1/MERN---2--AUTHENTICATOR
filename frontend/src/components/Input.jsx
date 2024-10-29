58e9a12819d4c2aeb0155a58e5e33c515e69e4e1
AKKK1 <akakichachava1@gmail.com>
2024-10-29 21:28:38 +0100

    FrontSide

---
+++ frontend/src/components/Input.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Input = (icon: Icon, ...props) => {
+  return <div className="relative mb-6">
+    <div>
+        <Icon className="size 5 text-green-500/>
+    </div>
+  </div>;
+};
+
+export default Input;
