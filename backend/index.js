3ef746d706c512d40954a0dedbc2d84b5a8f3e2c
AKKK1 <akakichachava1@gmail.com>
2024-11-12 04:11:41 +0100

    bolo

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,15 @@ import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors"; 
 
 dotenv.config();
 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));  // allows cross-origin requests: req.headers.origin
 
 app.use(express.json());   //allows to parce incoming request: req.body
 
